d6558cf7bac26b19fba6dcadfee3d1acbfec3852
root <personne.nico@gmail.com>
2018-11-20 15:23:42 +0100

    edit

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-$name = 'Personne';
-$lastName = 'Nicolas';
-$age = 25;
-settype($age, 'integer');
-echo  'Bonjour' . $lastname . $firstname . ', tu as ' . $age . ' ans';
- ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <a href="http://PHP/"><button type="button" class="btn btn-danger" name="button">Accueil</button></a>
+  <?php
+  $name = 'Personne';
+  $lastName = 'Nicolas';
+  $age = 25;
+  settype($age, 'integer');
+  echo  'Bonjour'.' '.$lastName.' '.$name.', tu as '.$age.' ans.';
+  ?>
+</body>
+</html>
